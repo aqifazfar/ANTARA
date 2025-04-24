b7e91ff3dd864eab377f1f09caeb64154334244c
4q1fWhiteHat <aqifazfar740@gmail.com>
2025-04-25 01:57:24 +0800

    Add mediator yet,it's needed to analyze the usage of singleton in serialization & deserialization class

--- cmake/antara-config.cmake
+++
@@ -1,6 +0,0 @@
-get_filename_component(ANTARA_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-
-if(NOT TARGET ANTARA::antaralib)
-    include("${ANTARA_CMAKE_DIR}/antara-targets.cmake")    
-endif()
-
--- cmake/antara-targets.cmake
+++
@@ -1,5 +0,0 @@
-add_library(ANTARA::antaralib IMPORTED)
-
-set_target_properties(ANTARA::antaralib
-                      PROPERTIES
-                      INTERFACE_INCLUDE_DIRECTORIES "${_IMPORT_PREFIX}/include")
